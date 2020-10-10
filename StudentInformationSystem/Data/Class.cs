f32285ef110edc8a8cb7a2b20e1b5c005d790300
Dan <Chunk_77@hotmail.com>
2020-10-10 08:20:02 -0700

    Added random class generator and populates in Grades page
    * From master
    
    * Add basic random class generator for display
    
    * Revert "From master"
    
    This reverts commit a330afd24ccb1fcf210a019669935caee1849cf9.
    
    * Revert "Revert "From master""
    
    This reverts commit ceea72ad493180e2d329c677b20e1061f4cbf288.
    
    * Revert "Revert "Revert "From master"""
    
    This reverts commit 9652f2bb6b02cef27f96970fe43df889363df18c.
    
    * Revert "Revert "Revert "Revert "From master""""
    
    This reverts commit ce883d0a5df6a15698d5518fdf871feef86df29c.

---
+++ StudentInformationSystem/Data/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace StudentInformationSystem.Data
+{
+    public class Class
+    {
+        public string ClassName { get; set; }
+        public string Description { get; set; }
+        public int Units { get; set; }
+        public double Grade { get; set; }
+    }
+}
---
+++ StudentInformationSystem/Data/StudentGradesService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentInformationSystem.Data
+{
+    public class StudentGradesService
+    {
+        private static readonly string[] Classes = new[]
+        {
+            "Chemistry", "Biology", "Physics", "History", "English"
+        };
+
+        public Task<Class[]> GetGradesAsync()
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new Class
+            {
+                ClassName = Classes[rng.Next(Classes.Length)],
+                Description = "Intro Class",
+                Units = rng.Next(3, 5),
+                Grade = rng.NextDouble() + 3.0
+            }).ToArray());
+        }
+    }
+}
