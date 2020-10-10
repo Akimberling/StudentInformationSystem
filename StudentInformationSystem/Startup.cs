48982aa6f5917f8fc3fb4bd016b9654116e66c08
Dan <Chunk_77@hotmail.com>
2020-10-09 22:17:35 -0700

    Add basic random class generator for display

--- StudentInformationSystem/Startup.cs
+++ StudentInformationSystem/Startup.cs
@@ -29,6 +29,7 @@ namespace StudentInformationSystem
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<StudentGradesService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
